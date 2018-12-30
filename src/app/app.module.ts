ea20d966dc7583971f63b22de25316b07b9427bd
meghna300 <guptameghna300@gmail.com>
2018-12-30 19:10:55 +0530

    make playstore section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { UpdateComponent } from './update/update.component';
+import { DownloadComponent } from './download/download.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    UpdateComponent
+    UpdateComponent,
+    DownloadComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
